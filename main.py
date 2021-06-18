66cace408562aa8a9ef5cbbe32bcffdca9225b3e
christian.collaguazo <christian.collaguazom@gmail.com>
2021-06-18 17:30:31 -0500

    Ultimo commit

--- main.py
+++ main.py
@@ -73,3 +73,8 @@ def update(todo_id, done):
 
     return redirect(url_for('hello'))
     
+
+
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
